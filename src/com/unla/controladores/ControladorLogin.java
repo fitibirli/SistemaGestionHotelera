0ff21f1a06378da2b48cdef779125fc1eb677fb0
DBNick1 <ne_espinosa@hotmail.com>
2016-05-13 14:53:24 -0300

    index login

---
+++ src/com/unla/controladores/ControladorLogin.java
@@ -0,0 +1,32 @@
+package com.unla.controladores;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.unla.datos.Login;
+public class ControladorLogin extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws
+	ServletException, IOException {
+		
+		try {
+		//LoginABM lbm1 = new LoginABM();
+		String usuario = request.getParameter("usuario");
+		String contrasenia = request.getParameter("clave");
+		//Login login = new Login(usuario,contrasenia, null);
+		request.setAttribute("usuario", usuario);
+		request.setAttribute("contrasenia", contrasenia);
+		request.getRequestDispatcher("/bienvenido.jsp").forward(request, response);;
+
+		} catch (Exception e) {
+			response.sendError(500, "");
+		}
+
+	}
+}
